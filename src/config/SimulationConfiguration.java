a5bb170ed03b8526a5b9d09de1145de6a6e313bb
Kaan Yıldırım <kyildirim14@ku.edu.tr>
2019-09-29 22:33:34 +0300

    False positive configurations base

--- src/config/SimulationConfiguration.java
+++ src/config/SimulationConfiguration.java
@@ -2,6 +2,25 @@ package config;
 
 public class SimulationConfiguration {
 
-	//TODO Implement.
+	/**
+	 * If <code>true</code>, then the entire simulation uses one single rate for false positive rate defined by <code>{@link SimulationConfiguration#FALSE_POSITIVE_RATE FALSE_POSITIVE_RATE}</code>.
+	 */
+	private static boolean USE_SINGLE_FALSE_POSITIVE_RATE = true;
+	/**
+	 * If <code>true</code>, and <code>{@link SimulationConfiguration#USE_SINGLE_FALSE_POSITIVE_RATE USE_SINGLE_FALSE_POSITIVE_RATE}</code> is <code>false</code>, then the simulation uses automatically adjusting false positive rates in correlation with the size of a <code>{@link Domain}</code>.
+	 */
+	private static boolean USE_AUTO_ADJUSTING_FALSE_POSITIVE_RATES = false;
+	/**
+	 * If <code>{@link SimulationConfiguration#USE_AUTO_ADJUSTING_FALSE_POSITIVE_RATES USE_AUTO_ADJUSTING_FALSE_POSITIVE_RATES}</code> is <code>true</code> this is the upper limit for the false positive rate.
+	 */
+	private static float FALSE_POSITIVE_RATE_MAXIMUM = 5f;
+	/**
+	 * If <code>{@link SimulationConfiguration#USE_AUTO_ADJUSTING_FALSE_POSITIVE_RATES USE_AUTO_ADJUSTING_FALSE_POSITIVE_RATES}</code> is <code>true</code> this is the lower limit for the false positive rate.
+	 */
+	private static float FALSE_POSITIVE_RATE_MINIMUM = 1f;
+	/**
+	 * If <code>{@link SimulationConfiguration#USE_SINGLE_FALSE_POSITIVE_RATE USE_SINGLE_FALSE_POSITIVE_RATE}</code> is <code>true</code> this is the false positive rate.
+	 */
+	private static float FALSE_POSITIVE_RATE = 2f;
 	
 }
