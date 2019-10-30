9c02b22fa300a9a386722b52726a961143d3d944
Ahmet Uysal <auysal16@ku.edu.tr>
2019-10-30 13:30:05 +0300

    Implement graph visualization for AS Topology

--- src/test/Playground.java
+++ src/test/Playground.java
@@ -14,9 +14,13 @@ public class Playground {
 //		NetworkToGraphConverter.convertNetwork(nt).display();
 
 		AutonomousSystemTopology ast = DataReader.readAutonomousSystemTopologyFromFile("src/dataTexts/AS-topology.txt", "src/dataTexts/transAs.txt");
-
 		System.out.println("Num routes: " + ast.getRouteSet().size());
 		System.out.println("Num ASs: " + ast.getAutonomousSystems().size());
+		var graph = NetworkToGraphConverter.convertAutonomousSystemTopology(ast);
+		System.out.println("Num node: " + graph.getNodeSet().size());
+		System.out.println("Num edge: " + graph.getEdgeCount());
+		graph.display();
+
 	}
 	
 }
