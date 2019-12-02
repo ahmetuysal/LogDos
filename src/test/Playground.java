ecefa199f338e7abe2e094b3c2f4595b4f27dc51
Ahmet Uysal <auysal16@ku.edu.tr>
2019-12-02 13:25:11 +0300

    Change false positive rates for simulation

--- src/test/Playground.java
+++ src/test/Playground.java
@@ -22,23 +22,34 @@ public class Playground {
 		Random rg = new Random();
 		int range = availableNodes.size();
 		long startTime = System.currentTimeMillis();
-		double attack = 3000000; //3Gbps in kbps
-		int numAttacker = 1000;
-		double reachingAttack = 0;
-		for(int i = 0; i < numAttacker; i++) {
-			int start = availableNodes.get(rg.nextInt(range));
-			int target = availableNodes.get(rg.nextInt(range));
-			System.out.println(start + "->" + target);
-			var path = ast.findPathBetweenAutonomousSystemsBFS2(start, target);
-			int length =  path == null ? Integer.MAX_VALUE : path.size();
-			System.out.println("Distance: " + length);
-			reachingAttack += (attack * Math.pow(0.05, length));
-			// System.out.println(i);
+		double[] totalAttacks = { 1000000, 2000000, 3000000};
+		int[] numAttackers = {100, 500, 1000, 1500, 2000};
+		double[] falsePositiveRates = {0.0001D, 0.001D, 0.01D, 0.05D };
+
+		for (int numAttacker : numAttackers ) {
+			for (double totalAttack : totalAttacks) {
+				for (double falsePositiveRate : falsePositiveRates) {
+					double reachingAttack = 0;
+					double attack = totalAttack / numAttacker;
+
+					for(int i = 0; i < numAttacker; i++) {
+						int start = availableNodes.get(rg.nextInt(range));
+						int target = availableNodes.get(rg.nextInt(range));
+						// System.out.println(start + "->" + target);
+						var path = ast.findPathBetweenAutonomousSystemsBFS2(start, target);
+						int length =  path == null ? Integer.MAX_VALUE : path.size();
+						// System.out.println("Distance: " + length);
+						reachingAttack += (attack * Math.pow(falsePositiveRate, Math.floor(length/2)));
+						// System.out.println(i);
+					}
+
+					System.out.println("# Attacker: " + numAttacker + " totalAttack: " + totalAttack + " fpRate: " + falsePositiveRate + " reaching: " + reachingAttack);
+				}
+			}
 		}
+
 		long endTime = System.currentTimeMillis(); 
         System.out.println((endTime - startTime) / 1000 + "s");
-        System.out.println("Total Attack Rate = " + (attack * numAttacker) + "kbps");
-        System.out.println("Reaching Attack Rate = " + reachingAttack + "kbps");
 		
 		/*
 		ExecutorService bfsPool = Executors.newFixedThreadPool(100);
