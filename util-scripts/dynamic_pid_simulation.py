5f3ea977e8d1d160875e948ce2dc5afde6cb1811
Ahmet Uysal <auysal16@ku.edu.tr>
2020-01-23 04:06:08 +0300

    Implement dynamic pid simulation with exponentially distributed length between events

--- util-scripts/dynamic_pid_simulation.py
+++ util-scripts/dynamic_pid_simulation.py
@@ -1,15 +1,31 @@
-from scipy.stats import poisson
+from numpy.random import exponential
 import matplotlib.pyplot as plt
 
 num_attackers = 100
-learning_rate = 50
+learning_rate = 1
 update_period = 60
 attack_per_attacker = 20
 
 simulation_length = 200
-x = range(simulation_length)
-y = [attack_per_attacker *
-     int(num_attackers * poisson.cdf(i % update_period, learning_rate)) for i in x]
+
+x = []
+y = []
+
+current_time = 0
+current_number_of_learned_paths = 0
+
+while current_time < simulation_length:
+    time_to_learn = exponential(1 / learning_rate)
+    if current_number_of_learned_paths < num_attackers:
+        current_number_of_learned_paths += 1
+
+    if current_time % update_period > (current_time + time_to_learn) % update_period:
+        current_number_of_learned_paths = 0
+
+    current_time += time_to_learn
+    x.append(current_time)
+    y.append(current_number_of_learned_paths * attack_per_attacker)
 
 plt.plot(x, y)
+
 plt.show()
