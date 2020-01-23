842f7fa661992e980c2be1dc2e07f41319954eab
Ahmet Uysal <auysal16@ku.edu.tr>
2020-01-23 17:39:14 +0300

    Implemented final dPID simulation data generation script

--- util-scripts/dynamic_pid_simulation.py
+++ util-scripts/dynamic_pid_simulation.py
@@ -1,31 +1,34 @@
 from numpy.random import exponential
-import matplotlib.pyplot as plt
 
 num_attackers = 100
-learning_rate = 1
-update_period = 60
+learning_rates = [1, 2, 3, 5, 8]
+update_periods = [60, 120, 240]
 attack_per_attacker = 20
 
-simulation_length = 200
+simulation_length = 500
 
-x = []
-y = []
 
-current_time = 0
-current_number_of_learned_paths = 0
+for update_period in update_periods:
+    for learning_rate in learning_rates:
+        x = []
+        y = []
 
-while current_time < simulation_length:
-    time_to_learn = exponential(1 / learning_rate)
-    if current_number_of_learned_paths < num_attackers:
-        current_number_of_learned_paths += 1
-
-    if current_time % update_period > (current_time + time_to_learn) % update_period:
+        current_time = 0
         current_number_of_learned_paths = 0
 
-    current_time += time_to_learn
-    x.append(current_time)
-    y.append(current_number_of_learned_paths * attack_per_attacker)
+        while current_time < simulation_length:
+            time_to_learn = exponential(1 / learning_rate)
+            if current_number_of_learned_paths < num_attackers:
+                current_number_of_learned_paths += 1
 
-plt.plot(x, y)
+            if current_time % update_period > (current_time + time_to_learn) % update_period:
+                current_number_of_learned_paths = 0
 
-plt.show()
+            current_time += time_to_learn
+            x.append(current_time)
+            y.append(current_number_of_learned_paths * attack_per_attacker)
+
+        with open('dPID-LR' + str(learning_rate) + '-UP' + str(update_period) + '.dat', 'w') as file:
+            file.write('time(seconds)\taggragete Attack(Mbps)\n')
+            for i in range(len(x)):
+                file.write(f'{x[i]}\t{y[i]}\n')
